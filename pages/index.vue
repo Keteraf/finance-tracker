657caed677d824863fa909be455441ab57884954
Raffaele Longo Elia <raffaele.longo.elia@gmail.com>
2023-11-21 14:20:50 +0100

    Refactoring index page

--- pages/index.vue
+++ pages/index.vue
@@ -8,10 +8,10 @@
         </div>
     </section>
     <section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 sm:gap-16 mb-10">
-        <Trend color="green" title="Income" :amount="incomeTotal" :last-amount="3000" :loading="isLoading" />
-        <Trend color="red" title="Expense" :amount="expenseTotal" :last-amount="4000" :loading="isLoading" />
-        <Trend color="red" title="Investments" :amount="2000" :last-amount="3000" :loading="isLoading" />
-        <Trend color="green" title="Savings" :amount="4000" :last-amount="2000" :loading="isLoading" />
+        <Trend color="green" title="Income" :amount="incomeTotal" :last-amount="3000" :loading="pending" />
+        <Trend color="red" title="Expense" :amount="expenseTotal" :last-amount="4000" :loading="pending" />
+        <Trend color="red" title="Investments" :amount="2000" :last-amount="3000" :loading="pending" />
+        <Trend color="green" title="Savings" :amount="4000" :last-amount="2000" :loading="pending" />
     </section>
 
     <section class="flex justify-between mb-10">
@@ -23,16 +23,16 @@
         </div>
         <div>
             <UButton icon="i-heroicons-plus-circle" color="white" variat="solid" label="Add" @click="isOpen = true" />
-            <TransactionModal v-model="isOpen" @saved="refreshTransactions()" />
+            <TransactionModal v-model="isOpen" @saved="refresh()" />
         </div>
 
     </section>
 
-    <section v-if="!isLoading">
-        <div v-for="(transactionsOnDay, date) in transactionsGroupByDate" :key="date" class="mb-10">
+    <section v-if="!pending">
+        <div v-for="(transactionsOnDay, date) in byDate" :key="date" class="mb-10">
             <DailyTransactionSummary :date="date" :transactions="transactionsOnDay" />
             <Transaction v-for="transaction in transactionsOnDay" :key="transaction.id" :transaction="transaction"
-                @deleted="refreshTransactions()" />
+                @deleted="refresh()" />
         </div>
     </section>
     <section v-else>
@@ -44,69 +44,21 @@
 import { transactionViewOptions } from '~/constants';
 
 const selectedView = ref(transactionViewOptions[1])
-const supabase = useSupabaseClient()
-const transactions = ref([])
-const isLoading = ref(false)
+
 const isOpen = ref(false)
 
-const income = computed(
-    () => transactions.value.filter(t => t.type === 'Income')
-)
-
-const expense = computed(
-    () => transactions.value.filter(t => t.type === 'Expense')
-)
-
-const incomeCount = computed(() => income.value.length)
-const expenseCount = computed(() => expense.value.length)
-
-const incomeTotal = computed(
-    () => income.value.reduce((sum, transaction) => sum + transaction.amount, 0)
-)
-
-const expenseTotal = computed(
-    () => expense.value.reduce((sum, transaction) => sum + transaction.amount, 0)
-)
-
-const fetchTransactions = async () => {
-    isLoading.value = true
-
-    try {
-        const { data } = await useAsyncData('transactions', async () => {
-            const { data, error } = await supabase
-                .from('transactions')
-                .select()
-                .order('created_at', {
-                    ascending: false
-                })
-
-            if (error) return []
-
-            return data
-        })
-        return data.value
-    } finally {
-        isLoading.value = false
+const {
+    pending,
+    refresh,
+    transactions: {
+        incomeCount,
+        expenseCount,
+        incomeTotal,
+        expenseTotal,
+        grouped: { byDate }
     }
-}
+} = useFetchTransactions()
 
-const refreshTransactions = async () => transactions.value = await fetchTransactions()
+await refresh()
 
-await refreshTransactions()
-
-const transactionsGroupByDate = computed(() => {
-    let grouped = {}
-
-    for (const transaction of transactions.value) {
-        const date = new Date(transaction.created_at).toISOString().split('T')[0]
-
-
-        if (!grouped[date]) {
-            grouped[date] = []
-        }
-
-        grouped[date].push(transaction)
-    }
-    return grouped
-})
 </script>
